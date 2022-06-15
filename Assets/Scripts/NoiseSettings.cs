30ac1e1517d41d1445737ce869896cced07e36e5
Daionis <54008677+Exyde@users.noreply.github.com>
2022-06-15 12:29:45 +0200

    tweak

--- Assets/Scripts/NoiseSettings.cs
+++ Assets/Scripts/NoiseSettings.cs
@@ -10,21 +10,22 @@ public class NoiseSettings {
         Simple, Ridged
     };
 
+    public FilterType _filterType;
+
     //Hiding based on the enum index
-    [ConditionalHide("filterType", 0)]
+    [ConditionalHide("_filterType", 0)]
     public SimpleNoiseSettings simpleNoiseSettings;
-    [ConditionalHide("filterType", 1)]
+    [ConditionalHide("_filterType", 1)]
     public RidgedNoiseSettings ridgedNoiseSettings;
 
-    public FilterType _filterType;
 
     [System.Serializable]
     public class SimpleNoiseSettings
     {
-        [Range(0, 8)] public float _minValue;
+        [Range(0, 2)] public float _minValue = .2f;
         [Range(0, 8)] public int _numLayers = 1;
         [Range(0, 8)] public float _strenght = 1;
-        [Range(0, 4)] public float _baseRoughness;
+        [Range(0, 4)] public float _baseRoughness = 1;
         [Range(0, 8)] public float _roughness = 2;
         [Range(0, 1)] public float _persistence = .5f;
         public Vector3 _center;
@@ -33,7 +34,7 @@ public class NoiseSettings {
     [System.Serializable]
     public class RidgedNoiseSettings : SimpleNoiseSettings   
     {
-        [Range(0, 8)] public float _weightMultiplier = 1f;
+        [Range(0, 1)] public float _weightMultiplier = 1f;
     }
 
 
